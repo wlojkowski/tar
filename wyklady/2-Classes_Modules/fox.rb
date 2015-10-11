401d93648b2340a116f5d67943f0b5bbb8354be0
Włodek Bzyl <wbzyl@mbook.local>
2015-10-11 11:31:57 +0200

    updade wyklady

--- wyklady/2-Classes_Modules/fox.rb
+++ wyklady/2-Classes_Modules/fox.rb
@@ -1,18 +1,23 @@
+# fox.rb
 class Animal
+  attr_accessor :health
+
   def initialize
     @health = 0
   end
 end
 
+# an example
 class Fox < Animal
-  attr_accessor :health
   def self.breeds
     ['snow fox', 'desert fox']
   end
+
   def initialize
     super
     @health += 5
   end
+
   def eat(food)
     if likes_food?(food)
       @health += 5
@@ -20,26 +25,31 @@ class Fox < Animal
       @health += 1
     end
   end
+
   def bark
     puts 'wrrrr' if @health > 0
     @health -= 1
   end
 
   private
+
   def likes_food?(food)
     food == 'chunky bacon'
   end
 end
 
+# something to mix
 module Invisibility
   def hide
     @visible = false
   end
+
   def show
     @visible = true
   end
 end
 
+# use Invisibility
 class Fox
   attr_accessor :visible
   include Invisibility
