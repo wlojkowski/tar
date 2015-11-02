64b2b92d87a03a3a49b5ecd5ecd4b975b99dce3f
Włodek Bzyl <wbzyl@mbook.local>
2015-11-02 19:24:34 +0100

    update

--- labs/03-rspec/lib/part2.rb
+++ labs/03-rspec/lib/part2.rb
@@ -8,6 +8,7 @@ def starts_with_vowel?(s)
   VOWELS.include?(s.capitalize[0])
 end
 
+# http://stackoverflow.com/questions/2339695/how-to-convert-a-string-or-integer-to-binary-in-ruby
 def binary_multiple_of_4?(s)
   s[-2..-1] == '00'
 end
