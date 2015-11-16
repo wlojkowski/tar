606231389a27ea089d717f289c257f9e72dd0d9c
Włodek Bzyl <wbzyl@mbook.local>
2015-11-16 22:25:45 +0100

    update spec/part2_spec.rb

--- labs/03-rspec/spec/part2_spec.rb
+++ labs/03-rspec/spec/part2_spec.rb
@@ -8,7 +8,7 @@ describe '#hello' do
     expect { hello('Testing') }.not_to raise_error
   end
 
-  it 'The hello method prints the correct string' do
+  it 'method prints the correct string' do
     expect(hello('Dan').class).to eq(String)
     expect(hello('Dan'))
       .to eq('Hello, Dan'), 'Incorrect results for input: \"Dan\"'
@@ -20,7 +20,7 @@ describe '#starts_with_vowel?' do
     expect { starts_with_vowel?('b') }.not_to raise_error
   end
 
-  it 'The starts_with_vowel? method returns the correct boolean' do
+  it 'method returns the correct boolean' do
     expect(starts_with_vowel?('asdfgh'))
       .to be(true), 'Incorrect results for input: \"asdfgh\"'
     expect(starts_with_vowel?('Veeeeeeee'))
@@ -33,7 +33,7 @@ describe '#binary_multiple_of_4?' do
     expect { binary_multiple_of_4?('yes') }.not_to raise_error
   end
 
-  it 'The binary_multiple_of_4? method returns the correct boolean' do
+  it 'returns the correct boolean' do
     expect(binary_multiple_of_4?('111111101'))
       .to be(false), 'Incorrect results for input: \"111111101\"'
     expect(binary_multiple_of_4?('1010101010100'))
