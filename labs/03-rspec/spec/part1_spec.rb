4abc7ce7988d2f4125fb5ba9bf13513db1328aab
Włodek Bzyl <wbzyl@mbook.local>
2015-11-17 10:44:55 +0100

    update 03-rspec

--- labs/03-rspec/spec/part1_spec.rb
+++ labs/03-rspec/spec/part1_spec.rb
@@ -3,39 +3,41 @@ SimpleCov.start
 
 require 'part1'
 
-describe '#sum' do
-  it 'should be defined' do
-    expect { sum([1, 3, 4]) }.not_to raise_error
+RSpec.describe 'part1.rb' do
+  describe '#sum' do
+    it 'should be defined' do
+      expect { sum([1, 3, 4]) }.not_to raise_error
+    end
+
+    it 'returns the correct sum' do
+      expect(sum([1, 2, 3, 4, 5])).to be_a_kind_of Fixnum
+      expect(sum([1, 2, 3, 4, 5])).to eq(15)
+      expect(sum([1, 2, 3, 4, -5])).to eq(5)
+    end
   end
 
-  it 'returns the correct sum' do
-    expect(sum([1, 2, 3, 4, 5])).to be_a_kind_of Fixnum
-    expect(sum([1, 2, 3, 4, 5])).to eq(15)
-    expect(sum([1, 2, 3, 4, -5])).to eq(5)
-  end
-end
+  describe '#max_2_sum' do
+    it 'should be defined' do
+      expect { max_2_sum([1, 2, 3]) }.not_to raise_error
+    end
 
-describe '#max_2_sum' do
-  it 'should be defined' do
-    expect { max_2_sum([1, 2, 3]) }.not_to raise_error
-  end
-
-  it 'returns the correct sum' do
-    expect(max_2_sum([1, 2, 3, 4, 5])).to be_a_kind_of(Fixnum)
-    expect(max_2_sum([1, 2, 3, 4, 100])).to eq(104)
-    expect(max_2_sum([1, -2, -3, -4, -5])).to eq(-1)
-  end
-end
-
-describe '#sum_to_n?' do
-  it 'should be defined' do
-    expect { sum_to_n?([1, 2, 3], 4) }.not_to raise_error
+    it 'returns the correct sum' do
+      expect(max_2_sum([1, 2, 3, 4, 5])).to be_a_kind_of(Fixnum)
+      expect(max_2_sum([1, 2, 3, 4, 100])).to eq(104)
+      expect(max_2_sum([1, -2, -3, -4, -5])).to eq(-1)
+    end
   end
 
-  it 'returns the correct value' do
-    expect(sum_to_n?([1, 2, 3, 4, 5], 5)).to be true
-    expect(sum_to_n?([1, 2, 5, 6, 7, 8], 3)).to be true
-    expect(sum_to_n?([100, 50, 50, 2, 100, 4, 5], 100)).to be true
-    expect(sum_to_n?([1, 2, 3, 4, 5], -3)).to be false
+  describe '#sum_to_n?' do
+    it 'should be defined' do
+      expect { sum_to_n?([1, 2, 3], 4) }.not_to raise_error
+    end
+
+    it 'returns the correct value' do
+      expect(sum_to_n?([1, 2, 3, 4, 5], 5)).to be true
+      expect(sum_to_n?([1, 2, 5, 6, 7, 8], 3)).to be true
+      expect(sum_to_n?([100, 50, 50, 2, 100, 4, 5], 100)).to be true
+      expect(sum_to_n?([1, 2, 3, 4, 5], -3)).to be false
+    end
   end
 end
--- labs/03-rspec/spec/part2_spec.rb
+++ labs/03-rspec/spec/part2_spec.rb
@@ -3,40 +3,42 @@ SimpleCov.start
 
 require 'part2'
 
-describe '#hello' do
-  it 'should be defined' do
-    expect { hello('Testing') }.not_to raise_error
+RSpec.describe 'part2.rb' do
+  describe '#hello' do
+    it 'should be defined' do
+      expect { hello('Testing') }.not_to raise_error
+    end
+
+    it 'method prints the correct string' do
+      expect(hello('Dan').class).to eq(String)
+      expect(hello('Dan'))
+        .to eq('Hello, Dan'), 'Incorrect results for input: \"Dan\"'
+    end
   end
 
-  it 'method prints the correct string' do
-    expect(hello('Dan').class).to eq(String)
-    expect(hello('Dan'))
-      .to eq('Hello, Dan'), 'Incorrect results for input: \"Dan\"'
-  end
-end
+  describe '#starts_with_vowel?' do
+    it 'should be defined' do
+      expect { starts_with_vowel?('b') }.not_to raise_error
+    end
 
-describe '#starts_with_vowel?' do
-  it 'should be defined' do
-    expect { starts_with_vowel?('b') }.not_to raise_error
-  end
-
-  it 'method returns the correct boolean' do
-    expect(starts_with_vowel?('asdfgh'))
-      .to be(true), 'Incorrect results for input: \"asdfgh\"'
-    expect(starts_with_vowel?('Veeeeeeee'))
-      .to be(false), 'Incorrect results for input: \"Veeeeeeee\"'
-  end
-end
-
-describe '#binary_multiple_of_4?' do
-  it 'should be defined' do
-    expect { binary_multiple_of_4?('yes') }.not_to raise_error
+    it 'method returns the correct boolean' do
+      expect(starts_with_vowel?('asdfgh'))
+        .to be(true), 'Incorrect results for input: \"asdfgh\"'
+      expect(starts_with_vowel?('Veeeeeeee'))
+        .to be(false), 'Incorrect results for input: \"Veeeeeeee\"'
+    end
   end
 
-  it 'returns the correct boolean' do
-    expect(binary_multiple_of_4?('111111101'))
-      .to be(false), 'Incorrect results for input: \"111111101\"'
-    expect(binary_multiple_of_4?('1010101010100'))
-      .to be(true), 'Incorrect results for input: \"1010101010100\"'
+  describe '#binary_multiple_of_4?' do
+    it 'should be defined' do
+      expect { binary_multiple_of_4?('yes') }.not_to raise_error
+    end
+
+    it 'returns the correct boolean' do
+      expect(binary_multiple_of_4?('111111101'))
+        .to be(false), 'Incorrect results for input: \"111111101\"'
+      expect(binary_multiple_of_4?('1010101010100'))
+        .to be(true), 'Incorrect results for input: \"1010101010100\"'
+    end
   end
 end
