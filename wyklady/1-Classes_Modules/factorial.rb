9776979170ad2fb177861974d9642fc022583c19
Wlodek Bzyl <matwb@univ.gda.pl>
2016-09-18 15:32:07 +0200

    update

--- wyklady/1-Classes_Modules/factorial.rb
+++ wyklady/1-Classes_Modules/factorial.rb
@@ -1,18 +1,23 @@
 def fact(n)
-  if n == 0
-    return 1
-  else
-    return n * fact(n - 1)
-  end
+  n.zero? ? 1 : n * fact(n - 1)
+  # if n.zero?
+  #   return 1
+  # else
+  #   return n * fact(n - 1)
+  # end
 end
 
 # add new method to builtin class
 class Integer
   def fact
-    if self.zero?
-      return 1
-    else
-      return self * (self - 1).fact
-    end
+    zero? ? 1 : self * (self - 1).fact
+    # if zero?
+    #   return 1
+    # else
+    #   return self * (self - 1).fact
+    # end
   end
 end
+
+print "fact(10) = #{fact(10)}\n"
+print "10.fact = #{10.fact}\n"
