38e759c1899e1d71dbb80e29cc78611d07190e29
Wlodek Bzyl <matwb@univ.gda.pl>
2016-10-03 12:06:56 +0200

    update

--- labs/02-sample_scripts/pdf/lista.rb
+++ labs/02-sample_scripts/pdf/lista.rb
@@ -1,46 +1,48 @@
 # -*- coding: utf-8 -*-
 
-# mongoexport --db test --collection students -f last_name,first_name  --query '{class_name:"nosql"}' --csv | tail -n +2 | sort > nosql.csv
+# mongoexport --db test --collection students -f last_name,first_name  \
+#   --query '{class_name:'nosql'}' --csv | tail -n +2 | sort > nosql.csv
 
 require 'bundler/setup'
 
-require "prawn"
-require "prawn/table"
+require 'prawn'
+require 'prawn/table'
 
-require "csv"
-require "pp"
+require 'csv'
+require 'pp'
 
-Prawn::Document.generate("lista.pdf") do
-  font "/usr/share/fonts/dejavu/DejaVuSans.ttf"
+Prawn::Document.generate('lista.pdf') do
+  # font '/usr/share/fonts/dejavu/DejaVuSans.ttf'
+  font '/Users/wbzyl/.fonts/Roboto/Roboto-Regular.ttf'
   font_size 11
 
   if ARGV.length == 0
-    puts "Usage: ruby lista.rb NAME.CSV"
+    puts 'Usage: ruby lista.rb NAME.CSV'
     exit
   else
-    # csv_file = "asi2011.csv"
+    # csv_file = 'asi2011.csv'
     csv_file = ARGV[0]
   end
 
-  # font "#{Prawn::BASEDIR}/data/fonts/DejaVuSans.ttf"
+  # font '#{Prawn::BASEDIR}/data/fonts/DejaVuSans.ttf'
 
   csv_data = open(csv_file).readlines
   body = CSV.parse(csv_data.join)
 
-  nazwisko = make_cell :content => "nazwisko"
-  imie = make_cell :content => "imię"
-  repo_url = make_cell :content => "github login"
+  nazwisko = make_cell :content => 'nazwisko'
+  imie = make_cell :content => 'imię'
+  repo_url = make_cell :content => 'github login'
 
   index = 0
   counted = body.map do |row|
     index += 1
-    row.unshift (index < 10 ? " " : "") + index.to_s
-    row += [" ", " "]
+    row.unshift (index < 10 ? ' ' : '') + index.to_s
+    row += [' ', ' ']
   end
 
-  # counted.unshift ["0", "Bzyl", "Włodzimierz", "wbzyl/trekking.git", ""]
+  # counted.unshift ['0', 'Bzyl', 'Włodzimierz', 'wbzyl/trekking.git', '']
   # pp counted
-  counted.unshift ["", nazwisko, imie, repo_url, "podpis"]
+  counted.unshift ['', nazwisko, imie, repo_url, 'podpis']
 
   table(counted, :header => true,
      :width => 540,
@@ -50,7 +52,7 @@ Prawn::Document.generate("lista.pdf") do
 
 end
 
-puts "--> lista.pdf"
+puts '--> lista.pdf'
 
-puts "Prawn:DATADIR: #{Prawn::DATADIR}"
-# font "#{Prawn::DATADIR}/fonts/Times-Roman.afm"
+puts 'Prawn:DATADIR: #{Prawn::DATADIR}'
+# font '#{Prawn::DATADIR}/fonts/Times-Roman.afm'
