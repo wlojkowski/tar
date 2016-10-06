1598e7a1dc928939ad13d7b323f2628e57d5224e
Wlodek Bzyl <matwb@univ.gda.pl>
2016-10-06 16:06:18 +0200

    update

--- labs/03-rspec/spec/part1_spec.rb
+++ labs/03-rspec/spec/part1_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe 'part1.rb' do
     end
 
     it 'returns the correct sum' do
-      expect(sum([1, 2, 3, 4, 5])).to be_a_kind_of Fixnum
+      expect(sum([1, 2, 3, 4, 5])).to be_a_kind_of Integer
       expect(sum([1, 2, 3, 4, 5])).to eq(15)
       expect(sum([1, 2, 3, 4, -5])).to eq(5)
     end
@@ -24,7 +24,7 @@ RSpec.describe 'part1.rb' do
     end
 
     it 'returns the correct sum' do
-      expect(max_2_sum([1, 2, 3, 4, 5])).to be_a_kind_of(Fixnum)
+      expect(max_2_sum([1, 2, 3, 4, 5])).to be_a_kind_of(Integer)
       expect(max_2_sum([1, 2, 3, 4, 100])).to eq(104)
       expect(max_2_sum([1, -2, -3, -4, -5])).to eq(-1)
     end
