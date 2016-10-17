eb1f6fd38bf8c72986c8d92d9fec11c9cf296609
Wlodek Bzyl <matwb@univ.gda.pl>
2016-10-17 11:06:31 +0200

    update

--- labs/02-sample_scripts/pdf/lista.rb
+++ labs/02-sample_scripts/pdf/lista.rb
@@ -16,7 +16,7 @@ Prawn::Document.generate('lista.pdf') do
   font '/Users/wbzyl/.fonts/Roboto/Roboto-Regular.ttf'
   font_size 11
 
-  if ARGV.length == 0
+  if ARGV.length.zero?
     puts 'Usage: ruby lista.rb NAME.CSV'
     exit
   else
@@ -29,9 +29,9 @@ Prawn::Document.generate('lista.pdf') do
   csv_data = open(csv_file).readlines
   body = CSV.parse(csv_data.join)
 
-  nazwisko = make_cell :content => 'nazwisko'
-  imie = make_cell :content => 'imię'
-  repo_url = make_cell :content => 'github login'
+  nazwisko = make_cell content: 'nazwisko'
+  imie = make_cell content: 'imię'
+  repo_url = make_cell content: 'github login'
 
   index = 0
   counted = body.map do |row|
@@ -40,16 +40,19 @@ Prawn::Document.generate('lista.pdf') do
     row += [' ', ' ']
   end
 
-  # counted.unshift ['0', 'Bzyl', 'Włodzimierz', 'wbzyl/trekking.git', '']
+  # counted.unshift ['0', 'Bzyl', 'Wlodzimierz', 'wbzyl/trekking.git', '']
   # pp counted
   counted.unshift ['', nazwisko, imie, repo_url, 'podpis']
 
-  table(counted, :header => true,
-     :width => 540,
-     :column_widths => {0 => 32},
-     :cell_style => {:height => 36, :padding => [12, 6, 12, 6], :overflow => :shrink_to_fit}
+  table(
+    counted,
+    header: true,
+    width: 540,
+    column_widths: { 0 => 32 },
+    cell_style: {
+      height: 36, padding: [12, 6, 12, 6], overflow: :shrink_to_fit
+    }
   )
-
 end
 
 puts '--> lista.pdf'
